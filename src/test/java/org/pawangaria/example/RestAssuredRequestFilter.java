4e329223abdfac23c436535bc3f919fb69dbd1e5
Garia <Pawan.Garia@gfk.com>
2020-09-21 17:52:01 +0200

    Fixed the issue with the Log4j

--- src/test/java/org/pawangaria/example/RestAssuredRequestFilter.java
+++ src/test/java/org/pawangaria/example/RestAssuredRequestFilter.java
@@ -5,20 +5,20 @@ import io.restassured.filter.FilterContext;
 import io.restassured.response.Response;
 import io.restassured.specification.FilterableRequestSpecification;
 import io.restassured.specification.FilterableResponseSpecification;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RestAssuredRequestFilter implements Filter {
-    private static final Log log = LogFactory.getLog(RestAssuredRequestFilter.class);
+    private final static Logger logger = LogManager.getLogger(RestAssuredRequestFilter.class);
 
     @Override
     public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
         Response response = ctx.next(requestSpec, responseSpec);
         if (response.statusCode() != 200) {
-            log.error(requestSpec.getMethod() + " " + requestSpec.getURI() + " => " +
+            logger.error(requestSpec.getMethod() + " " + requestSpec.getURI() + " => " +
                     response.getStatusCode() + " " + response.getStatusLine());
         }
-        log.info(requestSpec.getMethod() + " " + requestSpec.getURI() + " \n Request Body =>" + requestSpec.getBody() + "\n Response Status => " +
+        logger.info(requestSpec.getMethod() + " " + requestSpec.getURI() + " \n Request Body =>" + requestSpec.getBody() + "\n Response Status => " +
                 response.getStatusCode() + " " + response.getStatusLine() + " \n Response Body => " + response.getBody().prettyPrint());
         return response;
     }
--- src/test/java/org/pawangaria/example/TestRestassuredLog.java
+++ src/test/java/org/pawangaria/example/TestRestassuredLog.java
@@ -1,15 +1,14 @@
 package org.pawangaria.example;
 
 import io.restassured.response.Response;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 
 public class TestRestassuredLog {
 
-    private final static Log logger = LogFactory.getLog(TestRestassuredLog.class);
+    private static final Logger logger = LogManager.getLogger(TestRestassuredLog.class);
 
     @Test
     public void makingRestAssuredLogsNoFilter() {
